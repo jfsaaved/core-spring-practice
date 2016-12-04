7893524f26d7dd66ff00ac836ab9ec101af8ab6a
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2016-12-03 19:09:09 -0500

    Adding Gradle

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import org.joda.time.LocalTime; 
+
 public class Main {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World, trying my hands on Spring!!");
-	}
- 
+	  public static void main(String[] args) {
+		    LocalTime currentTime = new LocalTime();
+		    System.out.println("Current time to check if Gradle is working: " + currentTime);
+		  }
 }
